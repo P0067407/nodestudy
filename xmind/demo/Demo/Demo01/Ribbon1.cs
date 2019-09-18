5af86aeabd41af0a18a9e6b1430a15be57738c0a
“P0067407 <13504269699@qq.com>
2019-09-19 06:11:55 +0800

    提交

---
+++ xmind/demo/Demo/Demo01/Ribbon1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace Demo01
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            var ftm = new TestForm();
+            ftm.ShowDialog();
+        }
+    }
+}
---
+++ xmind/demo/Demo/Demo01/TestForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo01
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            //System.Windows.Forms.MessageBox.Show("Hello World!");
+            var path = "D:\\code\\nodestudy\\xmind\\demo\\Demo\\Demo01\\00.xlsx";
+            //var app = new Microsoft.Office.Interop.Excel.Application();
+            //var wbs = app.Workbooks;
+            //System.Windows.Forms.MessageBox.Show("Hello World!");
+            //var wb = wbs.Add(path);
+            //System.Windows.Forms.MessageBox.Show("Hello World!");
+
+            //wb.Activate();
+            //Globals.ThisAddIn.Application.ActiveWorkbook.FullName;
+            Globals.ThisAddIn.Application.Workbooks.Add(path);
+        }
+    }
+}
